320f8c4e79ec08d273a5c3dbcc16dfd31278c885
Charissa Ramirez <chawissa@gmail.com>
2021-04-14 20:42:00 -0400

    Eventlistener added

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ const displayResult = document.querySelector("#result");
 let isEqualsPressed = false;
 let equation = 0;
 let checkForDecimal = "";
+
+calcKeys.addEventListener("click", (e) => {
+  if (!e.target.closest("button")) return;
+
+  const key = e.target;
+  const keyValue = key.textContent;
+  let inputDisplay = userInput.textContent;
+  const { type } = key.dataset;
+  const { previousKeyType } = calculator.dataset;
+
+  if (type === "number" && !isEqualsPressed) {
+  }
+});
